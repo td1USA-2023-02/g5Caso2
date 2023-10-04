3e57da3f98dea81acc7866fde670f118354b97c0
ruben dario giraldo <ruben.giraldo01@usa.edu.co>
2023-10-03 19:02:55 -0500

    segundo commit

--- data.py
+++ data.py
@@ -1,25 +1,31 @@
 import pandas as pd
+import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
 
-
-df = pd.read_excel('datos.xlsx')
+df = pd.read_excel('g5Caso2/datos.xlsx')
 
 df.to_csv('datos.csv', index=False)
 
 print(df.head())
 
-# Para analizar las ventas por producto:
-ventas_por_producto = df.groupby('Product Name')['Sales'].sum()
-sns.barplot(x=ventas_por_producto.index, y=ventas_por_producto.values)
+# Obtener información sobre los tipos de datos de cada columna
+print(df.dtypes)
 
-# Para analizar las ventas por cliente:
-ventas_por_cliente = df.groupby('Customer Name')['Sales'].sum()
-sns.barplot(x=ventas_por_cliente.index, y=ventas_por_cliente.values)
+# Filtrar las columnas que contienen variables categóricas
+categorical_cols = [col for col in df.columns if df[col].dtype == 'object']
 
-# Para analizar las ventas por región:
-ventas_por_region = df.groupby('Region')['Sales'].sum()
-sns.barplot(x=ventas_por_region.index, y=ventas_por_region.values)
+# Imprimir las columnas que contienen variables categóricas
+print(categorical_cols)
 
-# Para analizar las ventas por segmento:
-ventas_por_segmento = df.groupby('Segment')['Sales'].sum()
-sns.barplot(x=ventas_por_segmento.index, y=ventas_por_segmento.values)
+# Filtrar las columnas que contienen variables cuantitativas
+quantitative_cols = [col for col in df.columns if df[col].dtype != 'object']
+
+# Imprimir las columnas que contienen variables cuantitativas
+print(quantitative_cols)
+
+# Crear un gráfico de dispersión que muestre la relación entre las ventas y el descuento
+sns.scatterplot(x='Discount', y='Sales', data=df)
+
+# Mostrar el gráfico
+plt.show()
